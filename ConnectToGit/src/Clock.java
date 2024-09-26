04c16d9965c50d0b241503bd74d333fdc2f34ac0
coppe <coppe@LAPTOP-GSO1MMUL>
2024-09-26 10:42:00 -0400

    Sep 26 Classwork

--- ConnectToGit/src/Clock.java
+++ ConnectToGit/src/Clock.java
@@ -4,12 +4,11 @@ public class Clock {
 	private int hr;
 	private int min;
 	private int sec;
-	//constructor method (overloaded)
+	//constructor method (overloaded) Constructors must be called same as class
+	
 	public Clock() {
 		
-		hr=0;
-		min=0;
-		sec=0;
+		setTime(0,0,0);
 		
 	}
 	
@@ -18,6 +17,87 @@ public class Clock {
 		hr=hours;
 		min=minutes;
 		sec=seconds;
+		
+	}
+	//setter--mutator methods
+	public void setHour(int hours) {
+		
+		hr=hours;
+		
+	}
+	//getter--accessor methods
+	public int getHour() {
+		
+		return hr;
+		
+	}
+	//regular methods
+	public void setTime(int hr, int min, int sec) {
+		
+		if (hr>=0 && hr<24)
+			this.hr=hr;
+		else
+			this.hr=0;
+		if (min>=0 && min<60)
+			this.min=min;
+		else
+			this.min=0;
+		if (sec>=0 && sec<60)
+			this.sec=sec;
+		else
+			this.sec=0;
+		
+	}
+	
+	public void incrementHours() {
+		
+		hr++;
+		if (hr>23)
+			hr=0;
+		
+	}
+	
+	public void incrementMinutes() {
+		
+		min++;
+		if (min>59) {
+			min=0;
+			incrementHours();
+		}
+		
+	}
+	
+	public void printTime() {
+		
+		System.out.print("\n");
+		if (hr<10)
+			System.out.print(0);
+		System.out.print(hr);
+		System.out.print(":");
+		if (min<10)
+			System.out.print(0);
+		System.out.print(min);
+		System.out.print(":");
+		if (sec<10)
+			System.out.print(0);
+		System.out.print(sec);
+		
+	}
+	//to string: override
+	public String toString() {
+		
+		String pClock="hour: ";
+		
+		if (hr<10)
+			pClock+="0";
+		pClock+=""+hr+"\nminute: ";
+		if (min<10)
+			pClock+="0";
+		pClock+=""+min+"\nseconds: ";
+		if (sec<10)
+			pClock+="0";
+		pClock+=""+sec+"\n";
+		return pClock;
 		
 	}
 	
--- ConnectToGit/src/ClockDriver.java
+++ ConnectToGit/src/ClockDriver.java
@@ -1,10 +1,22 @@
+import java.util.Scanner;
 
 public class ClockDriver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner reader=new Scanner(System.in);
 		Clock myClock=new Clock();
-		Clock yourClock=new Clock(3,4,5);
+		Clock yourClock=new Clock(23,59,59);
+		myClock.setTime(1, 2, 3);
+		myClock.setHour(23);
+		System.out.println(yourClock.getHour());
+		System.out.println(myClock);
+		myClock.incrementHours();
+		System.out.println(yourClock);
+		yourClock.incrementMinutes();
+		System.out.println(yourClock);
+		yourClock.printTime();
+		myClock.printTime();
 
 	}
 
